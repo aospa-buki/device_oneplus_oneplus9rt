da0cd2b2da7a55a3854deb1223c6fe24165580fa
Lunarixus <Nathan@lunarixus.dev>
2023-08-04 21:54:38 +0300

    oneplus9rt: Set surfaceflinger blurs as expensive
    * Lowers blur quality and uses EXPENSIVE_RENDERING powerhints.
    
    Signed-off-by: Lunarixus <Nathan@lunarixus.dev>
    Change-Id: I8c8455e36416cdc5acdd25925444efd5a70dbb72

--- device.mk
+++ device.mk
@@ -153,7 +153,8 @@ PRODUCT_VENDOR_PROPERTIES += \
     vendor.display.primary_mixer_stages=9
 
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
-    ro.surface_flinger.supports_background_blur=1
+    ro.surface_flinger.supports_background_blur=1 \
+    ro.sf.blurs_are_expensive=1
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/display/display_id_4630946728207028354.xml:$(TARGET_COPY_OUT_VENDOR)/etc/displayconfig/display_id_4630946728207028354.xml
